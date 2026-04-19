b76ff65660c8ee95595a093c36be51c671a35d63
Jaime López <exorsnine@gmail.com>
2026-04-19 17:50:25 -0500

    Added dashboard and fixes

--- src/main/java/com/web/restaurante/controller/UsuarioController.java
+++ src/main/java/com/web/restaurante/controller/UsuarioController.java
@@ -1,4 +1,9 @@
 package com.web.restaurante.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
 public class UsuarioController {
 }
